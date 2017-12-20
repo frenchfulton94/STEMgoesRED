6e5a4d71ec53a7c3e9ba4fac5d0bab89b1838daf
FrenchFulton94 <frenchfultonjr@gmail.com>
2017-12-20 17:52:27 -0500

    Added Firebase

---
+++ Pods/Target Support Files/Pods-STEM goes RED/Pods-STEM goes RED-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STEM_goes_RED : NSObject
+@end
+@implementation PodsDummy_Pods_STEM_goes_RED
+@end
---
+++ Pods/Target Support Files/Pods-STEM goes REDUITests/Pods-STEM goes REDUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STEM_goes_REDUITests : NSObject
+@end
+@implementation PodsDummy_Pods_STEM_goes_REDUITests
+@end
